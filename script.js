4f87dc9e7f02cd97dcf022b7055e8f8203ffefa2
yadiel <yadield2889@tuckahoeschools.org>
2024-12-09 13:31:34 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let length = 4;
+let width = 3; 
+const rectangleArea = (length*width);
+let pie = 3.14;
+let radius2 = 8;
+let base = 12;
+let height = 30;
+const triangleArea =  (1/2*base*height);
+const circleArea = (pie*radius2);
+console.log(triangleArea);
+console.log(circleArea);
+console.log(rectangleArea);
+
+// function area(shape, measure1,measure2){
+    //if(shape === "rectangle"){
+    // return measure1 * measure2;
+//}// else if(shape === "triangle"){
+    // return 0.5 * measure1 * measure2;
+//} // else if(shape === "circle"){
+//return Math.PI * measure1 * measure2;
+//}// else{
+// return "Shape not recognized";
+//}
+
+//}
+
+
+//console.log(area("rectangle", 10, 5));
+
